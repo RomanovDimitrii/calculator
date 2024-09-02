b979623e6b7d171b84a5b9edb5bdc9ff01d17dc5
Dmitrii Romanov <RomDim197@gmail.com>
2024-09-02 23:39:46 +0300

    setup for ghpages

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import ghPages from 'vite-plugin-github-pages';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), ghPages()],
+  base: 'https://RomanovDimitrii.github.io/calculator'
   root: 'src',
   publicDir: 'public',
   build: {
